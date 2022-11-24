38259747340b126fb90d0ab176ccdf2253ba9762
Nicolas Z <zacchino.n@gmail.com>
2022-11-23 22:15:44 -0300

    Implemented the ability to choose additional price points (HL2, HLC3, HLCC4, OHLC4) on top of the 'normal' ones (Open, High, Low, Close). This is done by the newly implemented additional_pricepoints function, which returns a DataFrame containing these new pricepoints.
    the indicators_history function now checks if the user introduced one of these
    pricepoints that needs to be calculated, and uses the dataframe returned by additional_pricepoints if that's the case
    (instead of the df returned by self.history() for the common pricepoints)

--- lib/tickers/tickerindicators.py
+++ lib/tickers/tickerindicators.py
@@ -5,12 +5,19 @@ from plotly.subplots import make_subplots
 from ..analysis import indicators
 
 class TickerWithIndicators(yf.Ticker):
+    """
+    Child class of the yf.Ticker class. Intended to add additional functionality
+    over its parent class (mainly indicators)
 
+    Methods:
+    indicators_history()
+    additional_pricepoints()
+    """
     def __init__(self, ticker, session=None):
         super().__init__(ticker, session)
 
     def indicators_history(self, field='Close', sma_params=[50], ema_params=[20],bollinger_params=[20,2],
-    rsi_params=[14],macd_params=[26,12,9]):
+    rsi_params=[14],macd_params=[26,12,9], period='max'):
         """
         Creates a Pandas Dataframe with the most commonly used indicators
         (SMA, EMa, Bollinger bands, RSI, MACD)
@@ -23,16 +30,26 @@ class TickerWithIndicators(yf.Ticker):
         (Default = [20,2])
         :param rsi_params: list with the numeric parameters for the RSI (Default = [14])
         :params macd_params: list with the numeric parameters for the MACD (Default = [26,12,9])
+        :params period: how far back in time the dataframe should go. same valid
+        values as yf.Ticker.history() method (default 'max')
 
         :returns: a Pandas Dataframe with columns containing the indicator values and the corresponding
         date in the index
         """
-        valid_fields = {'Open', 'Close', 'High', 'Low'}
-        #TODO: Implement HL2, HLC3, HLCC4, OHLC4
-        if field not in valid_fields:
-            raise ValueError("Invalid price action field. Must be one of %r" %valid_fields)
+        valid_fields = {'standard': ['Open','High','Low','Close'],
+        'calculated': ['HL2','HLC3','HLCC4','OHLC4']}
 
-        base_prices=self.history('max')[field]
+        #Checking if the user introduced a valid price point
+        if field not in valid_fields['calculated'] and field not in valid_fields['standard']:
+            raise ValueError("Invalid price action field. Must be one of %r" %valid_fields)
+        
+        #If the user intoduced a pricepoint not available in history, it has to be calculated
+        #with the corresponding additional_pricepoints() method
+        if field in valid_fields['standard']:
+            base_prices=self.history(period)[field]
+        if field in valid_fields['calculated']:
+            base_prices= self.additional_pricepoints(period)[field]
+
         sma_df = indicators.simple_moving_average(base_prices,*sma_params)
         ema_df = indicators.exponential_moving_average(base_prices, *ema_params)
         boll_df = indicators.bollinger_bands(base_prices, *bollinger_params)
@@ -40,4 +57,34 @@ class TickerWithIndicators(yf.Ticker):
         macd_df = indicators.macd(base_prices, *macd_params)
         
         df = pd.concat([sma_df,ema_df,boll_df,rsi_df,macd_df], axis=1)
+        return df
+
+    def additional_pricepoints(self, period):
+        """
+        Returns a Pandas dataframe with additional pricepoints, calculated from the 
+        more commonly used Open, High, Low, and Close
+
+        :param period: a string that states how far back in time the dataframe should go.
+        Same valid values as the yf.Ticker.history() method
+
+        returns: a Pandas dataframe with the HL2, HLC3, HLCC4 and OHLC4 price points
+        """
+        history = self.history(period)
+        
+        price_open = history['Open']
+        price_close = history['Close']
+        price_high = history['High']
+        price_low = history['Low']
+
+        HL2 = (price_high+price_low)/2
+        HLC3 = (price_high+price_low+price_close)/3
+        HLCC4 = (price_high+price_low+price_close*2)/4
+        OHLC4 = (price_high+price_low+price_close+price_open)/4
+
+        df = pd.concat([HL2.rename('HL2'),
+        HLC3.rename('HLC3'),
+        HLCC4.rename('HLCC4'),
+        OHLC4.rename('OHLC4')],
+        axis=1)
+
         return df
