efcb64a1fa78e1106d574c1d4a0190596f5311b0
Nicolas Z <zacchino.n@gmail.com>
2022-12-02 11:42:00 -0300

    Hotfix: Removed obsolete import from tickerindicators

--- investing_companion/tickers/tickerindicators.py
+++ investing_companion/tickers/tickerindicators.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 import pandas as pd
-from ..indicators import indicators
 
 class TickerWithIndicators(yf.Ticker):
     """
--- tests/plot_test.py
+++ tests/plot_test.py
@@ -1,13 +1,17 @@
 from investing_companion.tickers import tickerindicators
 import yfinance as yf
 import pandas as pd
+import numpy as np
 from investing_companion import indicators
-from investing_companion.indicators import moving_averages, macd
+from investing_companion.indicators import moving_averages, macd, bollinger
 
 tsla = tickerindicators.TickerWithIndicators('TSLA')
-
 hist = tsla.history('max')
+bolb = bollinger.BollingerBands(hist['Close'])
 
+conditions = [bolb.df['BB_Upper'] <= hist['Close'], 
+              bolb.df['BB_Lower'] >= hist['Close']]
 
-mcd = macd.MACD(hist['Close'])
-print(mcd.get_latest_value('MACD'))
+choices = ['Buy','Sell']
+
+hist['Signal'] = np.select(conditions, choices, 0)
